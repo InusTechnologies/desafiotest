3845ad6c17b9d8373bffbc0f19f94a1d7a8b8132
artur.borges <inustec@gmail.com>
2025-03-24 10:00:16 -0300

    Restaurar arquivos deletados

--- springboot/springboot/src/main/java/com/teste/springboot/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.teste.springboot.repository;
-
-import com.teste.springboot.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
